14226a2a4f47f3a3c72ae1edcc4fb9d2c35762b5
Ganesh Ray <62338623+ganeshray@users.noreply.github.com>
2022-08-30 17:29:03 +0530

    meta tag find for youtube url

--- index.js
+++ index.js
@@ -5,32 +5,33 @@ async function getLink(url) {
         const response = await axios.get(url);
         const reshtml = await response;
         const html = reshtml.data;
-        const $ = cheerio.load(html);
-        const metaTag = $('meta')
-            var content = ''
-            var image = ''
-            var title = $('title').text()
-            var url = url
-            var siteName = $('title').text()
-            metaTag.each((i,item)=>{
-                var data = $(item).attr('name')
-                var property = $(item).attr('property')
-                if(data === 'description'|| property==='og:description'){
-                    content = $(item).attr('content')
-                }
-                else if (property == 'og:title') {
-                    title = $(item).attr('content')
-                }
-                else if (property == 'og:image') {
-                    image = $(item).attr('content')
-                }
-                else if (property == 'og:url') {
-                    url = $(item).attr('content')
-                }
-                else if (property == 'og:site_name') {
-                    siteName = $(item).attr('content')
-                }
-            })
+        const $ = cheerio.load(html)
+        const meta = $('meta')
+
+        var content = ''
+        var image = ''
+        var title = $('title').text()
+        var url = url
+        var siteName = $('title').text()
+        meta.each((i,item)=>{
+            var data = $(item).attr('name')
+            var property = $(item).attr('property')
+            if(data === 'description'|| property==='og:description'){
+                content = $(item).attr('content')
+            }
+            else if (property == 'og:title') {
+                title = $(item).attr('content')
+            }
+            else if (property == 'og:image') {
+                image = $(item).attr('content')
+            }
+            else if (property == 'og:url') {
+                url = $(item).attr('content')
+            }
+            else if (property == 'og:site_name') {
+                siteName = $(item).attr('content')
+            }
+        })
 
         return { url: url, sitename: siteName, title: title, content: content, image: image };
     } catch (error) {
