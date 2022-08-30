47f8620f95dcb6b9e3da3e12b18f09a925649e5f
Ganesh Ray <62338623+ganeshray@users.noreply.github.com>
2022-08-30 17:17:09 +0530

    Update index.js

--- index.js
+++ index.js
@@ -7,29 +7,31 @@ async function getLink(url) {
         const html = reshtml.data;
         const $ = cheerio.load(html);
         const metaTag = $('meta')
-        var content = ''
-        var image = ''
-        var title = ''
-        var url = ''
-        var siteName = ''
-        metaTag.each((key, item) => {
-            var property = $(item).attr('property')
-            if (property == 'og:title') {
-                title = $(item).attr('content')
-            }
-            else if (property == 'og:description') {
-                content = $(item).attr('content')
-            }
-            else if (property == 'og:image') {
-                image = $(item).attr('content')
-            }
-            else if (property == 'og:url') {
-                url = $(item).attr('content')
-            }
-            else if (property == 'og:site_name') {
-                siteName = $(item).attr('content')
-            }
-        })
+            var content = ''
+            var image = ''
+            var title = $('title').text()
+            var url = url
+            var siteName = $('title').text()
+            meta.each((i,item)=>{
+                var data = $(item).attr('name')
+                var property = $(item).attr('property')
+                if(data === 'description'|| property==='og:description'){
+                    content = $(item).attr('content')
+                }
+                else if (property == 'og:title') {
+                    title = $(item).attr('content')
+                }
+                else if (property == 'og:image') {
+                    image = $(item).attr('content')
+                }
+                else if (property == 'og:url') {
+                    url = $(item).attr('content')
+                }
+                else if (property == 'og:site_name') {
+                    siteName = $(item).attr('content')
+                }
+            })
+
         return { url: url, sitename: siteName, title: title, content: content, image: image };
     } catch (error) {
         console.error(error);
